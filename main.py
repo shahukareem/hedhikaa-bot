204a5f32246c5b779cdeab2d71911e357e24c675
kudanai <kudanai@gmail.com>
2021-01-18 17:56:22 +0500

    fixes to config and stuff

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from fastai.vision import *
 from dotenv import load_dotenv
-from os import getenv
+import os
+from uuid import uuid4
 
 path = os.path.dirname(__file__)
 
@@ -13,18 +14,49 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
 load_dotenv()
-token = getenv("bot_token")
-url = getenv("bot_url")
-bot = telegram.Bot(token=token)
+token = os.getenv("bot_token")
+url = os.getenv("bot_url")
+port = int(os.getenv("PORT", 5000))
+is_webhook = (os.getenv("enable_webhook", "False") == "True")  # for testing, set to false to use polling
+
+logging.info(f"Loading model from {path}")
 model = load_learner(path, 'model/hedhika-classifier.pkl')
-port = int(os.environ.get("PORT", 5000))
+bot = telegram.Bot(token=token)
 
 
 def start(update, context):
     update.message.reply_text(
         "Welcome to Hedhikaa Bot. Send a photo of hedhikaa (Bajiyaa, Gulha, Boakibaa or Masroshi) and this classifier will tell what's in the photo \n "
     )
+
+
+def predict(update, context):
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+
+    # generate random filname and download
+    f_name = f"{uuid4()}.jpg"
+    photo_file.download(f_name)
+
+    # buzz the user
+    logger.info("Photo of %s: %s", user.first_name, f_name)
+    update.message.reply_text(
+        'Got your photo 👍 please wait!'
+    )
+
+    # process the image
+    img = open_image(f_name)
+
+    preds, idx, output = model.predict(img)
+    prediction = dict({model.data.classes[i]: round(to_np(p) * 100, 2) for i, p in enumerate(output) if p > 0.2})
+    for key, value in prediction.items():
+        result = "Likely " + key + " with " + str(value) + " percentage"
+        update.message.reply_text(result)
+
+    # remove the temp file
+    os.remove(f_name)
 
 
 def main():
@@ -35,29 +67,18 @@ def main():
 
     dp.add_handler(MessageHandler(Filters.photo, predict))
 
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(port),
-                          url_path=token)
-    updater.bot.setWebhook("https://hedhikaa-bot.herokuapp.com/" + token)
-    updater.idle()
-
-
-def predict(update, context):
-    user = update.message.from_user
-    photo_file = update.message.photo[-1].get_file()
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-    update.message.reply_text(
-        'Got your photo 👍 please wait!'
-    )
-
-    img = open_image('user_photo.jpg')
-
-    preds, idx, output = model.predict(img)
-    prediction = dict({model.data.classes[i]: round(to_np(p) * 100, 2) for i, p in enumerate(output) if p > 0.2})
-    for key, value in prediction.items():
-        result = "Likely " + key + " with " + str(value) + " percentage"
-        update.message.reply_text(result)
+    if is_webhook:
+        hook_url = f"{url}{token}"
+        updater.start_webhook(
+            listen="127.0.0.1",
+            port=port,
+            url_path=token,
+            webhook_url=hook_url)
+        updater.bot.set_webhook(hook_url)
+        updater.idle()
+    else:
+        updater.start_polling()
+        updater.idle()
 
 
 if __name__ == '__main__':
